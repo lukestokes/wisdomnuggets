907b7ae5a32ebf2d5aa92964ad1a113d412bf9bd
Luke Stokes <luke.stokes@gmail.com>
2021-02-28 10:51:46 -0400

    Changing the color schemes as some were too bright. Adding sayings from Max Marmer.

--- wisdom_data.php
+++ wisdom_data.php
@@ -6,3 +6,4 @@ include "wisdom_data/fallacies.php";
 include "wisdom_data/principles.php";
 include "wisdom_data/meditations.php";
 include "wisdom_data/sayings.php";
+include "wisdom_data/patterns.php";
